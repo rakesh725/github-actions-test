e4954756577ae0b5679895beaadc1f409c7d348f
Ray Smith <rptechworks@yahoo.com>
2020-07-30 15:18:35 -0400

    fix printing name

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -7,7 +7,7 @@ try{
     //throw(new Error("some error message"));
 
     const name = core.getInput('who-to-greet');
-    console.log('Hello ${name}')
+    console.log(`Hello ${name}`)
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
